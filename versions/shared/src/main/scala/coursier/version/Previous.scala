3847e8cfb3ed79dad41cd48af9fbb0e309f4dce3
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-06-24 20:56:34 +0200

    Add Previous.previousStableVersion

---
+++ versions/shared/src/main/scala/coursier/version/Previous.scala
@@ -0,0 +1,34 @@
+package coursier.version
+
+object Previous {
+
+  private val tagPattern = "-[A-Za-z]".r
+
+  private def fromSimpleVersion(version: String): Option[String] = {
+
+    val stripped = tagPattern.findFirstMatchIn(version)
+      .fold(version)(m => version.take(m.start))
+
+    for {
+      idx <- Some(stripped.lastIndexOf('.'))
+      if idx >= 0
+      (base, lastPart) = stripped.splitAt(idx + 1)
+      if lastPart.nonEmpty && lastPart.forall(_.isDigit)
+      num = lastPart.toInt
+      if num > 0
+    } yield s"$base${num - 1}"
+  }
+
+  def previousStableVersion(version: String): Option[String] =
+    if (version.contains('+')) {
+      val stripped = version.takeWhile(_ != '+')
+      if (tagPattern.findAllMatchIn(stripped).isEmpty)
+        Some(stripped)
+          // assume versions like '0.0.0' aren't published
+          .filter(_.exists(c => c.isDigit && c != '0'))
+      else
+        fromSimpleVersion(stripped)
+    } else
+      fromSimpleVersion(version)
+
+}
---
+++ versions/shared/src/test/scala/coursier/version/PreviousTests.scala
@@ -0,0 +1,27 @@
+package coursier.version
+
+import utest._
+
+object PreviousTests extends TestSuite {
+
+  def check(version: String, expectedPrevious: String): Unit = {
+    val previousOpt = Previous.previousStableVersion(version)
+    assert(previousOpt.contains(expectedPrevious))
+  }
+  def checkEmpty(version: String): Unit = {
+    val previousOpt = Previous.previousStableVersion(version)
+    assert(previousOpt.isEmpty)
+  }
+
+  val tests = Tests {
+    test - check("0.1.2", "0.1.1")
+    test - check("1.1", "1.0")
+    test - check("1.1+3", "1.1")
+    test -  check("1.1-RC2", "1.0")
+
+    test - checkEmpty("1.0-RC2")
+    test - checkEmpty("1.0-RC2+43")
+    test - checkEmpty("0.0.0+3-70919203-SNAPSHOT")
+  }
+
+}
