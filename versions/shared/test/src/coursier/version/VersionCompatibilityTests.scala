f50e9a1179e010ab5e8e016f02859cd79d6063dc
Alex Archambault <alexandre.archambault@gmail.com>
2025-01-27 16:31:13 +0100

    Use newer test syntax

--- versions/shared/test/src/coursier/version/VersionCompatibilityTests.scala
+++ versions/shared/test/src/coursier/version/VersionCompatibilityTests.scala
@@ -19,69 +19,141 @@ object VersionCompatibilityTests extends TestSuite {
   }
 
   val tests = Tests {
-    "semver" - {
+    test("semver") {
 
       implicit val compat = VersionCompatibility.EarlySemVer
 
-      test - compatible("1.1.0", "1.2.3")
-      test - compatible("1.0.0", "1.2.3")
-      test - compatible("1.1.0", "1.2.3-RC1")
-      test - incompatible("1.2.3-RC1", "1.2.3-RC2")
+      test {
+        compatible("1.1.0", "1.2.3")
+      }
+      test {
+        compatible("1.0.0", "1.2.3")
+      }
+      test {
+        compatible("1.1.0", "1.2.3-RC1")
+      }
+      test {
+        incompatible("1.2.3-RC1", "1.2.3-RC2")
+      }
 
-      test - compatible("0.1.1", "0.1.2")
-      test - incompatible("0.1.1", "0.2.2")
+      test {
+        compatible("0.1.1", "0.1.2")
+      }
+      test {
+        incompatible("0.1.1", "0.2.2")
+      }
 
-      test - minimumCompatible("1.0.1", "1")
-      test - minimumCompatible("1.1.2", "1")
+      test {
+        minimumCompatible("1.0.1", "1")
+      }
+      test {
+        minimumCompatible("1.1.2", "1")
+      }
 
-      test - minimumCompatible("0.0.1", "0.0")
-      test - minimumCompatible("0.1.2", "0.1")
-      test - minimumCompatible("1.0.0", "1")
-      test - minimumCompatible("0.0.0", "0.0")
-      test - minimumCompatible("0.1.0", "0.1")
+      test {
+        minimumCompatible("0.0.1", "0.0")
+      }
+      test {
+        minimumCompatible("0.1.2", "0.1")
+      }
+      test {
+        minimumCompatible("1.0.0", "1")
+      }
+      test {
+        minimumCompatible("0.0.0", "0.0")
+      }
+      test {
+        minimumCompatible("0.1.0", "0.1")
+      }
     }
 
-    "semverspec" - {
+    test("semverspec") {
 
       implicit val compat = VersionCompatibility.SemVerSpec
 
-      test - compatible("1.1.0", "1.2.3")
-      test - compatible("1.0.0", "1.2.3")
-      test - compatible("1.1.0", "1.2.3-RC1")
-      test - incompatible("1.2.3-RC1", "1.2.3-RC2")
+      test {
+        compatible("1.1.0", "1.2.3")
+      }
+      test {
+        compatible("1.0.0", "1.2.3")
+      }
+      test {
+        compatible("1.1.0", "1.2.3-RC1")
+      }
+      test {
+        incompatible("1.2.3-RC1", "1.2.3-RC2")
+      }
 
-      test - incompatible("0.1.1", "0.1.2")
-      test - incompatible("0.1.1", "0.2.2")
+      test {
+        incompatible("0.1.1", "0.1.2")
+      }
+      test {
+        incompatible("0.1.1", "0.2.2")
+      }
 
-      test - minimumCompatible("1.0.1", "1")
-      test - minimumCompatible("1.1.2", "1")
+      test {
+        minimumCompatible("1.0.1", "1")
+      }
+      test {
+        minimumCompatible("1.1.2", "1")
+      }
 
-      test - minimumCompatible("0.0.1", "0.0.1")
-      test - minimumCompatible("0.1.2", "0.1.2")
-      test - minimumCompatible("1.0.0", "1")
-      test - minimumCompatible("0.0.0", "0.0.0")
-      test - minimumCompatible("0.1.0", "0.1.0")
+      test {
+        minimumCompatible("0.0.1", "0.0.1")
+      }
+      test {
+        minimumCompatible("0.1.2", "0.1.2")
+      }
+      test {
+        minimumCompatible("1.0.0", "1")
+      }
+      test {
+        minimumCompatible("0.0.0", "0.0.0")
+      }
+      test {
+        minimumCompatible("0.1.0", "0.1.0")
+      }
     }
 
-    "package versioning" - {
+    test("package versioning") {
 
       implicit val compat = VersionCompatibility.PackVer
 
-      test - incompatible("1.1.0", "1.2.3")
-      test - incompatible("1.0.0", "1.2.3")
-      test - incompatible("1.1.0", "1.2.3-RC1")
-      test - compatible("1.0.0", "1.0.1")
-      test - compatible("0.1.0", "0.1.0+foo")
+      test {
+        incompatible("1.1.0", "1.2.3")
+      }
+      test {
+        incompatible("1.0.0", "1.2.3")
+      }
+      test {
+        incompatible("1.1.0", "1.2.3-RC1")
+      }
+      test {
+        compatible("1.0.0", "1.0.1")
+      }
+      test {
+        compatible("0.1.0", "0.1.0+foo")
+      }
 
-      test - minimumCompatible("1.0.1", "1.0")
-      test - minimumCompatible("1.1.2", "1.1")
+      test {
+        minimumCompatible("1.0.1", "1.0")
+      }
+      test {
+        minimumCompatible("1.1.2", "1.1")
+      }
 
-      test - minimumCompatible("0.0.1", "0.0")
-      test - minimumCompatible("0.1.2", "0.1")
-      test - minimumCompatible("1.0.0", "1.0")
+      test {
+        minimumCompatible("0.0.1", "0.0")
+      }
+      test {
+        minimumCompatible("0.1.2", "0.1")
+      }
+      test {
+        minimumCompatible("1.0.0", "1.0")
+      }
     }
 
-    "all" - {
+    test("all") {
 
       val compatibilities = Seq(
         VersionCompatibility.EarlySemVer,
@@ -101,9 +173,15 @@ object VersionCompatibilityTests extends TestSuite {
           Predef.assert(!compatible, s"Expected '$selected' not to be compatible with '$wanted' per $compat")
         }
 
-      test - incompatible("1.1+", "1.2.3")
-      test - compatible("[1.1,1.3)", "1.2.3")
-      test - incompatible("[1.1,1.2)", "1.2.3")
+      test {
+        incompatible("1.1+", "1.2.3")
+      }
+      test {
+        compatible("[1.1,1.3)", "1.2.3")
+      }
+      test {
+        incompatible("[1.1,1.2)", "1.2.3")
+      }
     }
   }
 
--- versions/shared/test/src/coursier/version/VersionConstraintTests.scala
+++ versions/shared/test/src/coursier/version/VersionConstraintTests.scala
@@ -5,35 +5,35 @@ import utest._
 object VersionConstraintTests extends TestSuite {
 
   val tests = Tests {
-    "parse" - {
-      "empty" - {
+    test("parse") {
+      test("empty") {
         val c0 = VersionParse.versionConstraint("")
         assert(c0 == VersionConstraint.empty)
       }
-      "basicVersion" - {
+      test("basicVersion") {
         val c0 = VersionParse.versionConstraint("1.2")
         assert(c0 == VersionConstraint.fromPreferred("1.2", Version("1.2")))
       }
-      "basicVersionInterval" - {
+      test("basicVersionInterval") {
         val c0 = VersionParse.versionConstraint("(,1.2]")
         assert(c0 == VersionConstraint.fromInterval("(,1.2]", VersionInterval(None, Some(Version("1.2")), false, true)))
       }
-      "latestSubRevision" - {
+      test("latestSubRevision") {
         val c0 = VersionParse.versionConstraint("1.2.3-+")
         assert(c0 == VersionConstraint.fromInterval("1.2.3-+", VersionInterval(Some(Version("1.2.3")), Some(Version("1.2.3-max")), true, true)))
       }
-      "latestSubRevisionWithLiteral" - {
+      test("latestSubRevisionWithLiteral") {
         val c0 = VersionParse.versionConstraint("1.2.3-rc-+")
         assert(c0 == VersionConstraint.fromInterval("1.2.3-rc-+", VersionInterval(Some(Version("1.2.3-rc")), Some(Version("1.2.3-rc-max")), true, true)))
       }
-      "latestSubRevisionWithZero" - {
+      test("latestSubRevisionWithZero") {
         val c0 = VersionParse.versionConstraint("1.0.+")
         assert(c0 == VersionConstraint.fromInterval("1.0.+", VersionInterval(Some(Version("1.0")), Some(Version("1.0.max")), true, true)))
       }
     }
 
-    "interval" - {
-      "checkZero" - {
+    test("interval") {
+      test("checkZero") {
         val v103 = Version("1.0.3")
         val v107 = Version("1.0.7")
         val v112 = Version("1.1.2")
@@ -42,7 +42,7 @@ object VersionConstraintTests extends TestSuite {
         assert(c0.contains(v107))
         assert(!c0.contains(v112))
       }
-      "subRevision" - {
+      test("subRevision") {
         val v = Version("1.2.3-rc")
         val c0 = VersionInterval(Some(v),  Some(Version("1.2.3-rc-max")), true, true)
         assert(c0.contains(v))
@@ -51,37 +51,37 @@ object VersionConstraintTests extends TestSuite {
       }
     }
 
-    "repr" - {
-      "empty" - {
+    test("repr") {
+      test("empty") {
         val s0 = VersionConstraint.empty.generateString
         assert(s0 == "")
       }
-      "preferred" - {
+      test("preferred") {
         val s0 = VersionConstraint.fromPreferred("2.1", Version("2.1")).generateString
         assert(s0 == "2.1")
       }
-      "interval" - {
+      test("interval") {
         val s0 = VersionConstraint.fromInterval("(,2.1]", VersionInterval(None, Some(Version("2.1")), false, true)).generateString
         assert(s0 == "(,2.1]")
       }
     }
 
-    "merge" - {
-      * - {
+    test("merge") {
+      test {
         val s0 = VersionConstraint.merge(
           VersionParse.versionConstraint("[1.0,3.2]"),
           VersionParse.versionConstraint("[3.0,4.0)")).get.generateString
         assert(s0 == "[3.0,3.2]")
       }
 
-      * - {
+      test {
         val c0 = VersionConstraint.merge(
           VersionParse.versionConstraint("[1.0,2.0)"),
           VersionParse.versionConstraint("[3.0,4.0)"))
         assert(c0.isEmpty)
       }
 
-      * - {
+      test {
         val c0 = VersionConstraint.merge(
           VersionParse.versionConstraint("[1.0,2.0)"),
           VersionParse.versionConstraint("[3.0,4.0)"),
@@ -90,15 +90,15 @@ object VersionConstraintTests extends TestSuite {
       }
     }
 
-    "relaxedMerge" - {
-      * - {
+    test("relaxedMerge") {
+      test {
         val s0 = VersionConstraint.relaxedMerge(
           VersionParse.versionConstraint("[1.0,2.0)"),
           VersionParse.versionConstraint("[3.0,4.0)")).generateString
         assert(s0 == "[3.0,4.0)")
       }
 
-      * - {
+      test {
         val s0 = VersionConstraint.relaxedMerge(
           VersionParse.versionConstraint("[1.0,2.0)"),
           VersionParse.versionConstraint("[3.0,4.0)"),
